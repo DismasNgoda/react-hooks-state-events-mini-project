8e7fd049cafa44c85021f2f0cb6d6858e3a102cf
Dismas Ng'oda <dismas.ngoda@student.moringaschool.com>
2022-10-02 09:17:54 +0300

    Assignment

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({tasks, functionDeleteTasks}) {
   return (
     <div className="tasks">
       {/* display a list of tasks using Task component */}
+      {tasks.map((task) =>(
+      <Task key={task.text} text={task.text} functionDeleteTasks={functionDeleteTasks} category={task.category} />
+      ))}
     </div>
   );
 }
